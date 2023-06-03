88291bcac8d5cac9b06a19a6f5a4da8a7984d457
Phong Thieu <pthieu@gmail.com>
2023-06-03 00:47:37 -0400

    Config: add AWS SSM Params override into config

--- src/config.ts
+++ src/config.ts
@@ -1,26 +1,73 @@
 import 'dotenv/config';
+import {
+  GetParametersByPathCommand,
+  GetParametersByPathCommandInput,
+  SSMClient,
+} from '@aws-sdk/client-ssm';
 
 export interface Config {
+  readonly ENVIRONMENT: string;
+  readonly PROJECT_NAME: string;
   readonly PORT: number;
   readonly DATABASE_URL: string;
+  readonly AWS_REGION: string;
+}
+
+const PROJECT_NAME = 'ts-api-postgres-boilerplate';
+const ENVIRONMENT = process.env.ENVIRONMENT || 'development';
+const AWS_REGION = process.env.AWS_REGION || 'us-west-2';
+
+// XXX(Phong): Write this to whatever provider you want to use.
+// Also, need to create these params in infra, and then fill manually
+async function getCloudSecrets() {
+  const client = new SSMClient({ region: AWS_REGION });
+  const input: GetParametersByPathCommandInput = {
+    // XXX(Phong): this name needs to be created
+    Path: `/${PROJECT_NAME}/env/${ENVIRONMENT}`,
+    Recursive: true,
+    WithDecryption: true,
+  };
+  const command = new GetParametersByPathCommand(input);
+  const response = await client.send(command);
+
+  const secrets = Object.fromEntries(
+    response.Parameters?.map((p) => [p.Name?.split('/').pop(), p?.Value]) || [],
+  );
+
+  return secrets satisfies Config as Config;
+}
+
+// XXX(Phong): must inject  before the config gets frozen or erros will throw
+if (process.env.ENVIRONMENT === 'production') {
+  const cloudSecrets = await getCloudSecrets();
+  Object.entries(cloudSecrets).forEach(([key, value]) => {
+    process.env[key] = value;
+  });
 }
 
 const config: Config = Object.freeze({
-  PORT: parseInt(getEnvVariable('PORT', '9000'), 10),
+  ENVIRONMENT,
+  PROJECT_NAME,
+  PORT: parseInt(getEnvVariable<number>('PORT', 9000) as string, 10),
   DATABASE_URL: getEnvVariable('DATABASE_URL'),
+  AWS_REGION,
 });
 
-function getEnvVariable(name: string, defaultVal?: string): string {
+function getEnvVariable<T = string>(
+  name: string,
+  defaultValue?: T,
+): string | T {
   const val = process.env[name];
 
-  if (!val) {
-    if (defaultVal) {
-      return defaultVal;
-    }
-    throw new Error(`environment variable ${name} not found`);
+  if (val) {
+    return val;
   }
 
-  return val;
+  if (defaultValue) {
+    return defaultValue;
+  }
+
+  throw new Error(`Missing environment variable: ${name}`);
 }
 
 export default config;
